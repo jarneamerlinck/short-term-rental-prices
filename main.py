40ac8c98aebb52dacef09ab720b0be88b47c27be
jarneamerlinck-do <jarne.amerlinck@dotocean.eu>
2024-04-11 09:54:41 +0200

    Set best params for hydra

--- main.py
+++ main.py
@@ -119,11 +119,16 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(
+                    f"{config['main']['components_repository']}/test_regression_model",
+                ),
+                "main",
+                parameters={
+                    "test_dataset": "test_data.csv:latest",
+                    "mlflow_model": "random_forest_export:prod",
+                },
+            )
 
 
 if __name__ == "__main__":
